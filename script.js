d5b723744d79ff5cd5635029be690338e2e0e35b
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-18 08:50:15 -0600

    did .then and .catch

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ let p = new Promise((resolve, reject) => {
     }
 })
 
+// anything in .then will run for a resolve and .catch is what we get if it fails
+p.then((message) => {
+  console.log('Hi, I am the then ' + message)
+}).catch((message) => {
+  console.log('Hi, I am the catch ' + message)
+})
