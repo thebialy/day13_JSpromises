a86bdc4ce5596cb055ad811ce1364c4410d454dc
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-24 14:36:40 -0600

    fixed commenting for simple promise function

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-let p = new Promise((resolve, reject) => {
-    let a = 1 + 1
-    if (a == 2) {
-        resolve('It worked!')
-    } else {
-        reject('Rejected')
-    }
-})
+// Simple promise
+// let p = new Promise((resolve, reject) => {
+//     let a = 1 + 1
+//     if (a == 2) {
+//         resolve('It worked!')
+//     } else {
+//         reject('Rejected')
+//     }
+// })
 
 // anything in .then will run for a resolve and .catch is what we get if it fails
-p.then((message) => {
-  console.log('Hi, I am the then ' + message)
-}).catch((message) => {
-  console.log('Hi, I am the catch ' + message)
-})
+// p.then((message) => {
+//   console.log('Hi, I am the then ' + message)
+// }).catch((message) => {
+//   console.log('Hi, I am the catch ' + message)
+// })
 
 
 const imposterSyndrome = false
